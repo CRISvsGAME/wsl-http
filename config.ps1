0aa900331e6f1421e257ef49989bfcb820b200e4
cris <cris@crisvsgame.com>
2025-01-21 21:23:09 +0000

    WSL HTTP 0.0.2
    Add Config Script

--- config.ps1
+++ config.ps1
@@ -1,10 +1,3 @@
-# WSL HTTP
-
-## Config
-
-### config.ps1
-
-```powershell
 # Firewall Rule Variables
 $Env:Name = "Allow_Inbound_HTTP"
 $Env:DisplayName = "Allow Inbound HTTP"
@@ -13,4 +6,3 @@ $Env:Direction = "Inbound"
 $Env:Enabled = "True"
 $Env:LocalPort = "80"
 $Env:Protocol = "TCP"
-```
